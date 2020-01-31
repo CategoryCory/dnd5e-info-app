566dc56691cde7545b7d79a3003b68185351be40
Cory Coward <cory@Corys-MacBook-Pro.local>
2020-01-30 20:21:55 -0600

    Add explanation and About text to homepage

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="app">
         <div id="nav">
-            <router-link to="/monsters">Monsters</router-link>&nbsp;|
-            <router-link to="/about">About</router-link>
+            <router-link to="/">Home</router-link>&nbsp;|
+            <router-link to="/monsters">Monsters</router-link>
         </div>
         <router-view />
     </div>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container w-50 about">
+        <h2>Welcome to the DND5E Information App</h2>
+        <p>Please select a module in the menu above to get started!</p>
+        <h4 class="mt-4">About This App</h4>
+        <p>
+            This app was written using the Vue.js framework, and it utilizes Axios for API interactions.
+            The DND data displayed comes from the
+            <a
+                href="http://www.dnd5eapi.co/"
+            >DND5E API</a>.
+        </p>
+    </div>
+</template>
