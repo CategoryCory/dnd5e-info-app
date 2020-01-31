983f1147d4adbaf63d5a193f2d0c8d13a60959d3
Cory Coward <cory@Corys-MacBook-Pro.local>
2020-01-30 20:45:39 -0600

    Add logo to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container w-50 about">
-        <h2>Welcome to the DND5E Information App</h2>
+        <img alt="DND5E logo" src="../assets/dnd5e-logo.jpg" />
+        <h2 class="mt-3">Welcome to the DND5E Information App</h2>
         <p>Please select a module in the menu above to get started!</p>
         <h4 class="mt-4">About This App</h4>
         <p>
