4a9b6b6ae6791c240e0ec34c0be86962c4e6debf
Cory Coward <cory@Corys-MacBook-Pro.local>
2020-02-09 13:28:06 -0600

    Complete Spell Data table

--- src/components/SpellData.vue
+++ src/components/SpellData.vue
@@ -5,23 +5,86 @@
                 <h2 class="card-title">{{ this.spellData.name }}</h2>
                 <h5
                     class="card-subtitle mb-3 text-muted"
-                >{{ `Level ${this.spellData.level} Spell` }}</h5>
+                >{{ `Level ${this.spellData.level} ${this.spellClasses.join(", ")} Spell (${this.spellData.school.name})` }}</h5>
                 <hr />
                 <div class="container">
                     <div class="row mb-3">
+                        <div class="col-sm-6">
+                            <p class="card-text text-left">
+                                <span class="font-weight-bold">Range:</span>
+                                {{ this.spellData.range }}
+                            </p>
+                        </div>
+                        <div class="col-sm-6">
+                            <p class="card-text text-left">
+                                <span class="font-weight-bold">Ritual:&nbsp;</span>
+                                <span v-if="spellData.ritual">Yes</span>
+                                <span v-else>No</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-6">
+                            <p class="card-text text-left">
+                                <span class="font-weight-bold">Duration:</span>
+                                {{ this.spellData.duration }}
+                            </p>
+                        </div>
+                        <div class="col-sm-6">
+                            <p class="card-text text-left">
+                                <span class="font-weight-bold">Concentration:&nbsp;</span>
+                                <span v-if="spellData.concentration">Yes</span>
+                                <span v-else>No</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-6">
+                            <p class="card-text text-left">
+                                <span class="font-weight-bold">Casting Time:</span>
+                                {{ this.spellData.casting_time }}
+                            </p>
+                        </div>
+                        <div class="col-sm-6">
+                            <p class="card-text text-left">
+                                <span class="font-weight-bold">Components:&nbsp;</span>
+                                <span
+                                    v-if="spellData.components.length"
+                                >{{this.spellData.components.join(", ")}}</span>
+                                <span v-else>None</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row mb-3" v-if="spellData.material">
                         <div class="col-sm-12">
                             <p class="card-text text-left">
-                                <span class="font-weight-bold">Description:</span>
-                                {{ this.spellData.desc[0] }}
+                                <span class="font-weight-bold">Material:</span>
+                                {{ this.spellData.material }}
                             </p>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-12">
+                            <h5
+                                class="card-subtitle text-muted text-left mt-2 mb-2"
+                            >Spell Description</h5>
+                            <p
+                                class="card-text text-left"
+                                v-for="(description, index) in spellData.desc"
+                                v-bind:key="index"
+                            >{{description}}</p>
                         </div>
                     </div>
                     <div class="row mb-3" v-if="spellData.higher_level">
                         <div class="col-sm-12">
-                            <p class="card-text text-left">
-                                <span class="font-weight-bold">At higher levels:</span>
-                                {{ this.spellData.higher_level[0] }}
-                            </p>
+                            <h5
+                                class="card-subtitle text-muted text-left mt-2 mb-2"
+                            >At Higher Levels</h5>
+                            <p
+                                class="card-text text-left"
+                                v-for="(higherLevel, index) in spellData.higher_level"
+                                v-bind:key="index"
+                            >{{higherLevel}}</p>
                         </div>
                     </div>
                 </div>
@@ -44,6 +107,7 @@ export default {
     data() {
         return {
             spellData: {},
+            spellClasses: [],
             errors: []
         };
     },
@@ -54,6 +118,12 @@ export default {
                     `http://www.dnd5eapi.co/api/spells/${this.spell}`
                 );
                 this.spellData = response.data;
+
+                // Format spell classes
+                this.spellClasses = [];
+                this.spellData.classes.forEach(spellClass => {
+                    this.spellClasses.push(spellClass.name);
+                });
             } catch (error) {
                 this.errors.push(error);
             }
